8ec0a5000f7cec60d4d10bed6e452681da907112
Mikey-Mikey <mikeygmoore07@gmail.com>
2026-01-07 20:49:19 -0500

    first commit

---
+++ lua/autorun/client/fontpack.lua
@@ -0,0 +1,11 @@
+-- InitPostEntity is required since the TEXTSCREEN_REVAMPED table is not available before then.
+hook.Add( "InitPostEntity", "fontpack.AddFonts", function()
+    if not TEXTSCREEN_REVAMPED then
+        MsgC( Color( 255, 0, 0 ), "Textscreens Revamped is not installed!\n" )
+        return
+    end
+
+    -- Add your fonts here. They have to be named exactly the same as the font in your addon's resource/fonts folder.
+    -- If your font is a .otf you have to change the file extension to .ttf in your resource/fonts folder. It will not mess up the font.
+    TEXTSCREEN_REVAMPED.AddFont( "VCR OSD Mono" ) -- Without the .ttf
+end )
